788c9f363069c67b9aac873e91bdb36ad913ad7c
Ranjith Kanna <ranjithkanna@Mac.station>
2026-03-23 00:00:27 +0000

    updated app.py

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def category(category):
     return render_template('index.html', listings=listings, current_category=category)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
